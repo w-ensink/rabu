3bd177b6adc93ed9e072b7f410f0d9c1934a16a7
Wouter Ensink <46427708+w-ensink@users.noreply.github.com>
2022-11-11 13:57:15 +0100

    add doc comments to units

--- src/units.rs
+++ src/units.rs
@@ -4,9 +4,11 @@ use derive_more::{Add, AddAssign, Sub, SubAssign};
 use partial_min_max::{max, min};
 use serde::{Deserialize, Serialize};
 
+/// Represents a percentage, e.g. the export progress.
 #[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
 pub struct Percentage(pub f64);
 
+/// Represents a bit depth for an audio file.
 #[derive(Serialize, Deserialize)]
 pub enum BitDepth {
     Bits8,
@@ -16,6 +18,7 @@ pub enum BitDepth {
 }
 
 impl BitDepth {
+    /// converts the number of bits to an integer.
     pub fn to_u16(&self) -> u16 {
         match self {
             BitDepth::Bits8 => 8,
@@ -26,6 +29,7 @@ impl BitDepth {
     }
 }
 
+/// Represent seconds in audio domain.
 #[derive(
     Serialize,
     Deserialize,
@@ -42,19 +46,23 @@ impl BitDepth {
 pub struct Seconds(pub f64);
 
 impl Seconds {
+    /// Convert to samples using the given sample rate.
     pub fn to_samples(&self, sr: SampleRate) -> Samples {
         Samples((self.value() * sr.value() as f64) as u64)
     }
 
+    /// Gives back the raw value in f64.
     pub fn value(&self) -> f64 {
         self.0
     }
 
+    /// Returns itself as a time point.
     pub fn as_time_point(&self) -> TimePoint {
         TimePoint(*self)
     }
 }
 
+/// Represents samples in the audio domain.
 #[derive(
     Serialize,
     Deserialize,
@@ -73,44 +81,54 @@ impl Seconds {
 pub struct Samples(pub u64);
 
 impl Samples {
+    /// Gives back the raw value as `u64`.
     pub fn value(&self) -> u64 {
         self.0
     }
 
+    /// Converts to seconds using the given sample rate.
     pub fn to_seconds(&self, sr: SampleRate) -> Seconds {
         Seconds(self.value() as f64 / sr.value() as f64)
     }
 
+    /// Gives back the raw value as a `usize`.
     pub fn as_usize(&self) -> usize {
         self.value() as usize
     }
 
+    /// Gives back the raw value as a `f64`.
     pub fn as_f64(&self) -> f64 {
         self.value() as f64
     }
 }
 
+/// Represents a sample rate (in Hz.).
 #[derive(Serialize, Deserialize, Copy, Clone, Debug, PartialEq, Eq)]
 pub struct SampleRate(pub u32);
 
 impl SampleRate {
+    /// Gives back the raw value as a `u32`.
     pub fn value(&self) -> u32 {
         self.0
     }
 
+    /// Gives back the raw value as a `usize`.
     pub fn as_usize(&self) -> usize {
         self.value() as usize
     }
 
+    /// Gives back the raw value as a `f64`.
     pub fn as_f64(&self) -> f64 {
         self.value() as f64
     }
 
+    /// Gives back the raw value as a `u64`.
     pub fn as_u64(&self) -> u64 {
         self.value() as u64
     }
 }
 
+/// Represents a number of audio channels.
 #[derive(
     Serialize,
     Deserialize,
@@ -129,44 +147,54 @@ impl SampleRate {
 pub struct Channels(pub u32);
 
 impl Channels {
+    /// Gives back the raw value as a `u32`.
     pub fn value(&self) -> u32 {
         self.0
     }
 
+    /// Gives back the raw value as a `usize`.
     pub fn as_usize(&self) -> usize {
         self.value() as usize
     }
 
+    /// Gives back the raw value as a `f64`.
     pub fn as_f64(&self) -> f64 {
         self.value() as f64
     }
 
+    /// Gives back the raw value as a `u64`.
     pub fn as_u64(&self) -> u64 {
         self.value() as u64
     }
 }
 
+/// Represents a latency in the audio domain.
 #[derive(Serialize, Deserialize, Copy, Clone, Debug)]
 pub struct Latency(pub Seconds);
 
 impl Latency {
+    /// Gives back the raw value as a `f64`.
     pub fn value(&self) -> f64 {
         self.0.value()
     }
 
+    /// Creates a new latency from the given number of seconds.
     pub fn from_seconds(seconds: f64) -> Self {
         Self(Seconds(seconds))
     }
 }
 
+/// Represents a time point in the audio domain, e.g. the start position of a file.
 #[derive(Serialize, Deserialize, Copy, Clone, Debug, PartialEq, PartialOrd)]
 pub struct TimePoint(pub Seconds);
 
 impl TimePoint {
+    /// Gives back the time point in seconds
     pub fn as_seconds(&self) -> Seconds {
         self.0
     }
 
+    /// Creates a new time point from the given seconds.
     pub fn from_seconds(seconds: f64) -> Self {
         Self(Seconds(seconds))
     }
@@ -200,23 +228,28 @@ impl Sub<Self> for TimePoint {
     }
 }
 
+/// Represents a duration in the time domain, e.g. the length of a clip.
 #[derive(Serialize, Deserialize, Copy, Clone, Debug, PartialEq, PartialOrd)]
 pub struct Duration(pub Seconds);
 
 impl Duration {
+    /// Converts the duration to samples using the given sample rate.
     pub fn to_samples(&self, sr: SampleRate) -> Samples {
         self.0.to_samples(sr)
     }
 
+    /// Gives back the duration as seconds.
     pub fn as_seconds(&self) -> Seconds {
         self.0
     }
 
+    /// Creates a new duration from the given number of seconds.
     pub fn from_seconds(seconds: f64) -> Self {
         Self(Seconds(seconds))
     }
 }
 
+/// Represents a time section, e.g. the span of a clip in an arrangement.
 #[derive(Serialize, Deserialize, Copy, Clone, Debug, PartialEq, PartialOrd)]
 pub struct TimeSection {
     pub start: TimePoint,
@@ -224,6 +257,7 @@ pub struct TimeSection {
 }
 
 impl TimeSection {
+    /// Returns the overlap (if any) between this time section and another.
     pub fn get_overlap(&self, other: Self) -> Option<Self> {
         if self.end() <= other.start || other.end() <= self.start {
             return None;
@@ -235,6 +269,7 @@ impl TimeSection {
         Some(Self { start, duration })
     }
 
+    /// Returns the end point of this time section.
     pub fn end(&self) -> TimePoint {
         self.start + self.duration
     }
