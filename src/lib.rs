e97eeb8c160e7c5ac55c00fcf66734203b39017a
Wouter Ensink <46427708+w-ensink@users.noreply.github.com>
2022-12-20 22:41:05 +0100

    added crate wide doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+//! ## Examples
+//! Strongly typed units can easily be converted into each other:
+//! ```rust
+//! use rabu::units::{SampleRate, Samples, Seconds};
+//!
+//! let seconds = Seconds::from(3.0);
+//! let sample_rate = SampleRate::from(44100);
+//!
+//! let samples = seconds.to_samples(sample_rate);
+//!
+//! assert_eq!(samples, Samples::from(132_300));
+//! ```
+//!
+//! Audio buffers can be used in a way that makes sense:
+//! ```rust
+//! use rabu::buffer::Buffer;
+//! use rabu::units::{Channels, Samples};
+//!
+//! let mut buffer = Buffer::allocate(Channels::from(2), Samples::from(4));
+//!
+//! for channel in buffer.iter_chans_mut() {
+//!     for sample in channel.iter_mut() {
+//!         *sample = 1.0;
+//!     }
+//! }
+//! ```
+
 pub mod biquad;
 pub mod buffer;
 pub mod units;
