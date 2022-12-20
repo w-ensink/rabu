6b9e2fa94ed2e804238b4587b6f380edd2876384
Wouter Ensink <46427708+w-ensink@users.noreply.github.com>
2022-12-20 22:30:06 +0100

    added module wide doc comment to units module

--- src/units/mod.rs
+++ src/units/mod.rs
@@ -1,3 +1,8 @@
+//! This module contains a lot of strong types for units and quantities that are audio related.
+//! These types have their corresponding conversion functions that make sense for that particular
+//! type. For example: a `Seconds` object can be converted to a `Samples` object
+//! when given a `SampleRate` value.
+
 pub use bit_depth::BitDepth;
 pub use channels::Channels;
 pub use duration::Duration;
